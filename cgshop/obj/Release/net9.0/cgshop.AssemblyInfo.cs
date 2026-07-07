37c31cb66ce8be64c44709e120c136c50976ac05
LucaDev256 <82050504+LucaDev256@users.noreply.github.com>
2026-07-07 01:58:33 +0100

    Trial 1

--- cgshop/obj/Release/net9.0/cgshop.AssemblyInfo.cs
+++ cgshop/obj/Release/net9.0/cgshop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cgshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+089eb60d38c2c6d4acf873c3df1ba58ff38bbd59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+116b590a962d3b1571d90a30075fafeae07cd153")]
 [assembly: System.Reflection.AssemblyProductAttribute("cgshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cgshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
