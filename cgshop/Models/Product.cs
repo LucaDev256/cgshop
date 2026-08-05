3440fc4963061785d63af3be8bba09d9faafdd79
LucaDev256 <82050504+LucaDev256@users.noreply.github.com>
2026-08-05 17:00:08 +0100

    Add project files.

---
+++ cgshop/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace cgshop.Models
+{
+    public enum Marketplace { Amazon, Etsy, Tiktok, Vinted, Ebay}
+
+    public class Product(string name, float price, float weight, string uRLImage, string productURL, Marketplace marketplace)
+    {
+        public string Name { get; set; } = name;
+        public float Price { get; set; } = price;
+        public float Weight { get; set; } = weight;
+        public string URLImage { get; set; } = uRLImage;
+        public string ProductURL { get; set; } = productURL;
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Marketplace Marketplace { get; set; } = marketplace;
+    }
+}
---
+++ cgshop/Services/IconMapper.cs
@@ -0,0 +1,26 @@
+﻿using cgshop.Models;
+
+namespace cgshop.Services
+{
+    public class IconMapper
+    {
+        private readonly Marketplace _marketplace;
+
+        public static string GetIconName(Marketplace marketplace)
+        {
+            switch (marketplace)
+            {
+                case Marketplace.Amazon:
+                    return "amazon.png";
+                case Marketplace.Tiktok:
+                    return "tik-tok.png";
+                case Marketplace.Ebay:
+                    return "ebay.png";
+                case Marketplace.Vinted:
+                    return "vinted.png";
+                default:
+                    return "noimgfound.png";
+            }
+        }
+    }
+}
